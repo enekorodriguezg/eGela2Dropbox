1cf1a786d1fbc4fc4cf89fa38454fb8ce804e004
aimarlarriba <aimarlarriba12@gmail.com>
2026-05-15 09:59:23 +0200

    Punto 6 y nueva funcionalidad

--- Dropbox.py
+++ Dropbox.py
@@ -26,25 +26,21 @@ class Dropbox:
         self._root = root
 
     def local_server(self):
-        # por el puerto 8090 esta escuchando el servidor que generamos
         server_socket = socket(AF_INET, SOCK_STREAM)
         server_socket.bind((server_addr, server_port))
         server_socket.listen(1)
         print("\tLocal server listening on port " + str(server_port))
 
-        # recibe la redireccio 302 del navegador
         client_connection, client_address = server_socket.accept()
         peticion = client_connection.recv(1024)
         print("\tRequest from the browser received at local server:")
         print (peticion)
 
-        # buscar en solicitud el "auth_code"
         primera_linea =peticion.decode('UTF8').split('\n')[0]
         aux_auth_code = primera_linea.split(' ')[1]
         auth_code = aux_auth_code[7:].split('&')[0]
         print ("\tauth_code: " + auth_code)
 
-        # devolver una respuesta al usuario
         http_response = "HTTP/1.1 200 OK\r\n\r\n" \
                         "<html>" \
                         "<head><title>Proba</title></head>" \
@@ -56,8 +52,8 @@ class Dropbox:
 
         return auth_code
 
+
     def do_oauth(self):
-        # 1. Solicitar Autorización (Abrir el navegador)
         auth_url = "https://www.dropbox.com/oauth2/authorize"
         params_get = {
             "client_id": app_key,
@@ -65,15 +61,12 @@ class Dropbox:
             "redirect_uri": redirect_uri
         }
         
-        # urllib.parse.urlencode convierte el diccionario en formato query string (?clave=valor)
         url_completa = auth_url + "?" + urllib.parse.urlencode(params_get)
         print("Abriendo navegador para autorización OAuth...")
         webbrowser.open(url_completa)
 
-        # 2. Capturar el Código de Autorización (Tu servidor se queda escuchando aquí)
         auth_code = self.local_server()
 
-        # 3. Intercambiar el Código por el Token Definitivo
         token_url = "https://api.dropboxapi.com/oauth2/token"
         datos_post = {
             "code": auth_code,
@@ -84,7 +77,6 @@ class Dropbox:
         }
         
         print("Intercambiando authorization code por access token...")
-        # El POST requiere que enviemos los datos en el cuerpo de la petición (data)
         respuesta = requests.post(token_url, data=datos_post)
         
         if respuesta.status_code == 200:
@@ -95,44 +87,109 @@ class Dropbox:
         else:
             print(f"Error al obtener el token: {respuesta.status_code} - {respuesta.text}")
 
-        # Cerramos la ventana de login de Tkinter
         self._root.destroy()
 
-        self._root.destroy()
 
     def list_folder(self, msg_listbox):
         print("/list_folder")
         uri = 'https://api.dropboxapi.com/2/files/list_folder'
-        # https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
-        #############################################
-        # RELLENAR CON CODIGO DE LA PETICION HTTP
-        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
-        #############################################
+
+        headers = {
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/json'
+        }
+
+        ruta = "" if self._path == "/" else self._path
+
+        data = {
+            'path': ruta
+        }
+
+        respuesta = requests.post(uri, headers=headers, data=json.dumps(data))
+
+        if respuesta.status_code == 200:
+            contenido_json = respuesta.json()
+        else:
+            contenido_json = {}
+            print(respuesta.text)
 
         self._files = helper.update_listbox2(msg_listbox, self._path, contenido_json)
+
 
     def transfer_file(self, file_path, file_data):
         print("/upload")
         uri = 'https://content.dropboxapi.com/2/files/upload'
-        # https://www.dropbox.com/developers/documentation/http/documentation#files-upload
-        #############################################
-        # RELLENAR CON CODIGO DE LA PETICION HTTP
-        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
-        #############################################
+
+        headers = {
+            'Authorization': 'Bearer ' + self._access_token,
+            'Dropbox-API-Arg': json.dumps({'path': file_path, 'mode': 'add', 'autorename': True}),
+            'Content-Type': 'application/octet-stream'
+        }
+
+        respuesta = requests.post(uri, headers=headers, data=file_data)
+
+        if respuesta.status_code == 200:
+            print(respuesta.json())
+        else:
+            print(respuesta.text)
+
 
     def delete_file(self, file_path):
         print("/delete_file")
         uri = 'https://api.dropboxapi.com/2/files/delete_v2'
-        # https://www.dropbox.com/developers/documentation/http/documentation#files-delete
-        #############################################
-        # RELLENAR CON CODIGO DE LA PETICION HTTP
-        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
-        #############################################
+
+        headers = {
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/json'
+        }
+
+        data = {
+            'path': file_path
+        }
+
+        respuesta = requests.post(uri, headers=headers, data=json.dumps(data))
+
+        if respuesta.status_code == 200:
+            print(respuesta.json())
+        else:
+            print(respuesta.text)
+
 
     def create_folder(self, path):
         print("/create_folder")
-       # https://www.dropbox.com/developers/documentation/http/documentation#files-create_folder
-        #############################################
-        # RELLENAR CON CODIGO DE LA PETICION HTTP
-        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
-        #############################################
+        uri = 'https://api.dropboxapi.com/2/files/create_folder_v2'
+
+        headers = {
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/json'
+        }
+
+        data = {
+            'autorename': False,
+            'path': path
+        }
+
+        respuesta = requests.post(uri, headers=headers, data=json.dumps(data))
+
+        if respuesta.status_code == 200:
+            print(respuesta.json())
+        else:
+            print(respuesta.text)
+
+
+    def get_current_account(self):
+        print("/get_current_account")
+        uri = 'https://api.dropboxapi.com/2/users/get_current_account'
+
+        headers = {
+            'Authorization': 'Bearer ' + self._access_token
+        }
+
+        respuesta = requests.post(uri, headers=headers)
+
+        if respuesta.status_code == 200:
+            info = respuesta.json()
+            print("Nombre:", info.get('name', {}).get('display_name'))
+            print("Email:", info.get('email'))
+        else:
+            print(respuesta.text)
